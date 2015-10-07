b03baabd110a3efde3b244f63121501018dfff03
Glenn Hermans <glhermans@gmail.com>
2015-10-07 22:28:55 +0200

    Added basic authentication

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,16 @@
 |
 */
 
+
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
 Route::get('/', function () {
     return view('welcome');
 });

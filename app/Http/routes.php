38b5451f24bd0714df51a3899d23ba892589e780
Glenn Hermans <glhermans@gmail.com>
2015-11-11 07:00:07 +0100

    Added login and register blades

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>Time control</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" integrity="sha512-dTfge/zgoMYpP7QbHy4gWMEGsbsdZeCXz7irItjcC3sPUFtf0kuFbDz/ixG7ArTxmDjLXDmezHubeNikyKGVyQ==" crossorigin="anonymous">
+    <link href="{{ url('css/login.css') }}" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+<body>
+  <div class="container" style="margin-top:30px">
+  <div class="col-md-5 col-md-offset-3">
+    <div class="clearfix">&nbsp;</div>
+
+     @if (count($errors) > 0)
+    <div class="alert alert-danger">
+      <ul>
+        @foreach ($errors->all() as $error)
+          <li>{{ $error }}</li>
+        @endforeach
+      </ul>
+    </div>
+  @endif
+
+      <div class="panel panel-default">
+    <div class="panel-heading"><h3 class="panel-title"><strong>Sign In </strong></h3></div>
+    <form method="POST" action="/auth/login">
+    <div class="panel-body">
+    {!! csrf_field() !!}
+
+    <div class="form-group">
+        <label for="email" class="form-label">Email <span class="text-danger">*</span></label>
+        <input type="email" id="email" name="email" value="{{ old('email') }}" class="form-control">
+    </div>
+
+    <div>
+      <label for="password" class="form-label">Password <span class="text-danger">*</span></label>
+        <input type="password" id="password" name="password" class="form-control">
+    </div>
+
+    <div class="form-group">
+        <input type="checkbox" name="remember"> Remember Me
+    </div>
+ </div>
+ <div class="panel-footer">
+     <button type="submit" class="btn btn-sm btn-primary">Login</button>
+ </div>
+</form>
+</div>
+</div>
+</div>
+</body>
+</html>

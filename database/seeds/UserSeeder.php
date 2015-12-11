11d0e0a8087e01c2d5e36741dbba8cd45cf03acd
Glenn Hermans <glhermans@gmail.com>
2015-12-12 00:51:37 +0100

    Fixed typo

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -3,7 +3,7 @@ use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 use App\User as User;
-class UserTableSeeder extends Seeder {
+class UserSeeder extends Seeder {
 /**
    * Run the database seeds.
    *

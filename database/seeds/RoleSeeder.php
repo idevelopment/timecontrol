b4f21b1b2fd6fbfc955c0cc3832d16b125162750
Glenn Hermans <glhermans@gmail.com>
2015-12-13 10:37:09 +0100

    Fixed RoleSeeder

--- database/seeds/RoleSeeder.php
+++ database/seeds/RoleSeeder.php
@@ -12,14 +12,10 @@ class RoleSeeder extends Seeder {
 */
     public function run()
     {
-      DB::table('roles')->insert([['name' => 'Administrator', 'created_at' => date('Y-m-d H:i:s') ],
-                   ['name' => 'Agent', 'created_at' => date('Y-m-d H:i:s')],
-                   ['name' => 'Department manager', 'created_at' => date('Y-m-d H:i:s')],
-                   ['name' => 'Manager', 'created_at' => date('Y-m-d H:i:s')]
+      DB::table('roles')->insert([['name' => 'Administrator', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s') ],
+                   ['name' => 'Agent', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+                   ['name' => 'Department manager', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')],
+                   ['name' => 'Manager', 'created_at' => date('Y-m-d H:i:s'), 'updated_at' => date('Y-m-d H:i:s')]
                     ]);
-
-      DB::table('user_has_roles')->insert([['role_id' => '1', 'user_id' => '1']]);
-
-
     }
 }

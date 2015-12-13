a05743aa3b4a4215a3d4813f8774121abc28946f
Glenn Hermans <glhermans@gmail.com>
2015-12-13 17:41:41 +0100

    Added basic view for teams #3

---
+++ resources/views/teams/list.blade.php
@@ -0,0 +1,37 @@
+@extends('header')
+
+@section('content')
+<div class="page-header">
+<h2>Teams</h2>
+</div>
+<div class="well well-sm">
+<div class="btn-group">
+<button class="btn btn-sm btn-primary" onclick="location.href='{{url('staff/teams/create')}}';"><i class="fa fa-plus"></i> New team</button>
+<button class="btn btn-sm btn-danger">Remove team</button>
+</div>
+</div>
+
+<div class="panel panel-default">
+  <div class="panel-body">
+<table class="table">
+<thead>
+<th>Team</th>
+<th>Department</th>
+<th class="col-md-2">Total employee</th>
+<th class="col-md-1"></th>
+</thead>
+<tbody>
+@foreach($teams as $team_item)
+  <tr>
+    <td>{{ $team_item->name }}</td>
+    <td>{{ $team_item->department }}</td>
+    <td></td>
+    <td><a href="{{ url('staff/teams/edit') }}/{{ $team_item->id }}"><i class="fa fa-pencil"></i> <a href="{{ url('staff/teams/remove') }}/{{ $team_item->id }}"><i class="fa fa-times"></i></a></td>
+
+  </tr>
+  @endforeach
+</tbody>
+</table>
+</div>
+</div>
+@endsection

1f3d29276d62c643656444c900a86a12b66c2e55
Tjoosten <Tjoosten3@gmail.com>
2016-03-02 16:34:07 +0100

    Update on factories

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,17 +13,26 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-      'fname' => $faker->firstName,
-      'name' => $faker->lastName,
-      'email' => $faker->email,
-      'password' => bcrypt("demo123456"),
-      'remember_token' => str_random(10),
+        'fname' => $faker->firstName,
+        'name' => $faker->lastName,
+        'email' => $faker->email,
+        'password' => bcrypt("demo123456"),
+        'remember_token' => str_random(10),
     ];
 });
-    $factory->define(App\Teams::class, function (Faker\Generator $faker) {
-        return [
-          'name' => $faker->company,
-          'manager' => '1',
-          'description' => '',
-        ];
+
+$factory->define(App\Teams::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->company,
+        'manager' => '1',
+        'description' => '',
+    ];
+});
+
+$factory->define(App\Departments, function(Faker\Generator $faker) {
+    return [
+        'department_name' => $faker->name,
+        'department_manager' => $faker->name,
+        'department_description' => $faker->text,
+    ];
 });

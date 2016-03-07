cabc200dacb44bf4c8e7cd6e9486292e6b66bc46
Tjoosten <Tjoosten3@gmail.com>
2016-03-07 22:18:29 +0100

    Issue fix

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -10,11 +10,10 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
         $this->call(CountrySeeder::class);
-        $this->call(RoleSeeder::class);
         $this->call(UserSeeder::class);
 
         // Add demo users to play with
-        factory(App\User::class, 20)->create()->each(function($user) { $user->assignRole('Agent');});
+        factory(App\User::class, 20)->create();
         factory(App\Teams::class, 20)->create();
         factory(App\Departments::class, 20)->create();
         factory(App\Sick::class, 20)->create();               

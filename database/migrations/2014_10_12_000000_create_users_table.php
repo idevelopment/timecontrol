91e41db7e34db391725d5992388a6e04a917823f
Tjoosten <Tjoosten3@gmail.com>
2016-04-03 03:37:12 +0200

    Fix #160

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -42,8 +42,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-		DB::statement('SET FOREIGN_KEY_CHECKS = 0');
         Schema::drop('users');
-		DB::statement('SET FOREIGN_KEY_CHECKS = 1');
     }
 }

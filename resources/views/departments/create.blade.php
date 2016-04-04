8e3ce0559b75b9d0b8fd72a9fad4b3aeb55903ec
Glenn Hermans <glhermans@gmail.com>
2016-04-04 02:09:08 +0200

    Quickfix

--- resources/views/departments/create.blade.php
+++ resources/views/departments/create.blade.php
@@ -8,35 +8,38 @@
 <div class="col-md-12">
 <form action="{{ url('staff/departments/docreate') }}" id="simple_wizard" method="post" class="form-horizontal">
 {!! csrf_field() !!}
- <fieldset title="General info">
-  <legend class="hide">Basic settings</legend>
+ <fieldset title="{{Lang::get('departments.basic')}}">
+  <legend class="hide">{{Lang::get('departments.basicHelper')}}</legend>
    <div class="form-group">
-    <label for="department_name" class="col-md-2 control-label">Department name <span class="text-danger">*</span></label>
+    <label for="department_name" class="col-md-2 control-label">{{Lang::get('departments.name')}} <span class="text-danger">*</span></label>
     <div class="col-md-6">
      <input type="text" id="department_name" name="department_name" class="form-control">
+     <span id="helpBlock" class="help-block">{{Lang::get('departments.nameHelper')}}</span>     
     </div>
   </div>
 
 <div class="form-group">
- <label for="department_manager" class="col-md-2 control-label">Department manager <span class="text-danger">*</span></label>
+ <label for="department_manager" class="col-md-2 control-label">{{Lang::get('departments.manager')}} <span class="text-danger">*</span></label>
   <div class="col-md-6">
-   <select id="department_manager" name="department_manager" class="form-control">
+   <select id="department_manager" name="department_manager" class="form-control" multiple="">
     <option></option>
      @foreach($managers as $manager_data)
       <option value="{{$manager_data->id}}">{{$manager_data->fname}} {{$manager_data->name}}</option>
      @endforeach
     </select>
+     <span id="helpBlock" class="help-block">{{Lang::get('departments.departmentHelper')}}</span>         
    </div>
   </div>
 
 <div class="form-group">
- <label for="department_description" class="col-md-2 control-label">Description</label>
+ <label for="department_description" class="col-md-2 control-label">{{Lang::get('departments.description')}}</label>
  <div class="col-md-6">
   <textarea id="department_description" name="department_description" rows="10" class="form-control"></textarea>
+  <span id="helpBlock" class="help-block">{{Lang::get('departments.descriptionHelper')}}</span>     
  </div>
 </div>
 </fieldset>
-<fieldset title="Teams">
+<fieldset title="{{Lang::get('departments.teams')}}">
  <legend class="hide">Assign your existing teams or create a new one</legend>
 
   <div class="form-group">
@@ -59,8 +62,8 @@
   </div>
 
  </fieldset>
- <fieldset title="Notifications">
-  <legend class="hide">Configure notification settings</legend>
+ <fieldset title="{{Lang::get('departments.notifications')}}">
+  <legend class="hide">{{Lang::get('departments.notificationsHelper')}}</legend>
 
   <div class="form-group">
    <label for="teams" class="col-md-2 control-label">Account creation</label>
@@ -116,8 +119,45 @@
   </div>
  </div>
 
- </fieldset> 
-  <button type="submit" class="finish btn btn-primary">{{ Lang::get('app.save')}}</button>
+ </fieldset>
+
+<fieldset title="Workhours">
+<legend class="hide">Assign the workhours that are allowed for this department</legend>
+
+<table class="table" id="tab_logic">
+        <thead>
+          <tr >
+            <th class="text-center">
+              #
+            </th>
+            <th class="text-center">
+              Name
+            </th>
+            <th class="text-center">
+              Start hour
+            </th>
+            <th class="text-center">
+              End hour
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr id='addr0'>
+            <td class="text-center">1</td>
+            <td><input type="text" name='name0'  placeholder='Name' class="form-control"/></td>
+            <td><input type="text" name='start_time0' id='start_time' class="form-control"/></td>
+            <td><input type="text" name='stop_time0' id='stop_time' class="form-control"/></td>
+          </tr>
+          <tr id='addr1'></tr>
+        </tbody>
+      </table>
+  
+  <a id="add_row" class="btn btn-default pull-left">Add Row</a>&nbsp;
+  <a id='delete_row' class="btn btn-danger">Delete Row</a>
+ <div class="clearfix">&nbsp;</div>
+ </fieldset>
+ <div class="clearfix">&nbsp;</div>
+ <button type="submit" class="finish btn btn-primary">{{ Lang::get('app.save')}}</button>
   </form>
 </div>
 
@@ -125,7 +165,29 @@
   $(document).ready(function() {
     department_wizard.simple();
     department_wizard.steps_nb();
+  
+   var i=1;
+    $("#add_row").click(function(){
+    $('#addr'+i).html("<td class='text-center'>"+ (i+1) +"</td><td><input name='name"+i+"' type='text' placeholder='Name' class='form-control input-md'  /> </td><td><input  name='start"+i+"' type='text' id='start_time' class='form-control'></td><td><input  name='stop"+i+"' type='text' id='stop_time' class='form-control input-md'></td>");
+
+      $('#tab_logic').append('<tr id="addr'+(i+1)+'"></tr>');
+      i++; 
   });
+     $("#delete_row").click(function(){
+       if(i>1){
+     $("#addr"+(i-1)).html('');
+     i--;
+     }
+   });
+  
+   $('#start_time').datetimepicker({
+      format: 'hh:mm'
+    });
+
+   $('#stop_time').datetimepicker({
+     format: 'hh:mm'
+    });    
+ });
 
   department_wizard = {
     simple: function(){

b35a6c3683aed3da25d2f04d18a15c93b42004b9
Tjoosten <Tjoosten3@gmail.com>
2016-04-23 16:30:17 +0200

    Fix #145 -> Set backup method.

--- config/laravel-backup.php
+++ config/laravel-backup.php
@@ -145,8 +145,8 @@ return [
          * Here you can specify how emails should be sent.
          */
         'mail' => [
-            'from' => 'your@email.com',
-            'to'   => 'your@email.com',
+            'from' => config(timecontrol.email),
+            'to'   => config(timecontrol.email),
         ],
 
         /*

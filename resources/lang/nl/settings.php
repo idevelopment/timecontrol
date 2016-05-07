6dee1a08ce019a69bd06e7e5a34eab669045e7d1
Glenn Hermans <glhermans@gmail.com>
2016-05-07 14:39:41 +0000

    Quick update

---
+++ resources/lang/nl/settings.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+  // Basic settings
+  "basic_title" => "Basis instellingen",
+  "general_title" => "Algemeen",
+  "date_title" => "Datum & tijd",
+  "title" => "Applicatie titel",
+  "email" => "Applicatie email",
+  "date" => "Datum formaat",
+  "time" => "Tijd formaat",
+  "updateConfirmation" => "Instellingen zijn bewaard.",
+  "updateErrored" => "De instellingen zijn niet opgeslagen, controleer alles and probeer het opnieuw.",  
+
+  // Backup settings
+  "bacup_title" => "Backup instellingen",
+  "backup_db" => "Database verbinding",
+  "backup_db_helper" => "The name of the connections to the databases that should be part of the backup.",
+  "backup_include" => "Include directories",
+  "backup_include_helper" => "These directories or files will be included from the backup.",
+  "backup_exclude" => "Exclude directories",
+  "backup_exclude_helper" => "These directories or files will be excluded from the backup.",
+  "backup_store_all" => "Store all backups",
+  "backup_store_all_helper" => "The amount of days that all daily backups must be kept"
+];
---
+++ resources/lang/nl/tasks.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Tasks & scheduling Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines for the portal
+	|
+	*/
+
+	"schedule_type" => "Type",
+	"login_required" => "You must be logged in to use the portal",
+	"no_portal_account" => "This account does not have portal access",
+	"portal_user_blocked" => "This portal account has been disabled",
+	"sign_out" => "Sign out",
+	"sign_in" => "Sign in",
+	"forgot_password" => "Forgot password?",
+
+  /*
+	|--------------------------------------------------------------------------
+	| Absenteeism
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines for the absenteeism feature
+	|
+	*/
+    "sick_notification" => "Arbeidsongeschikt",
+	"new_sick_notification" => "Ziekte melding",
+	"sick_notification_register" => "Nieuwe ziektemelding",
+	"sick_notification_type" => "Type",
+	"sick_notification_employee" => "Personeelslid",
+	"sick_notification_created_by_user" => "Aangemaakt door",
+	"sick_notification_modified_by_user" => "Gewijzigd door",
+	"sick_notification_start" => "Van",
+	"sick_notification_stop" => "Tot",
+	"sick_notification_duplicate" => "This is already registered.",
+
+	/*
+	|--------------------------------------------------------------------------
+	| Widget
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines for the widget view
+	|
+	*/
+
+	"WidgetTitle" => "Recente taak aanvragen",
+	"WidgetCreated" => "Datum aangemaakt",
+	"WidgetType" => "Type",
+	"WidgetAssigned" => "Agent",
+	"WidgetStart" => "Start",
+	"WidgetStop" => "Stop",
+	"WidgetStatus" => "Status"
+	];
+
+

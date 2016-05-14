43bf3cb5fc9f18cd59756b880e2be49488e2b712
Tjoosten <Tjoosten3@gmail.com>
2016-05-14 03:05:43 +0200

    add vueJS active state in the holiday request form

--- public/js/interaction.js
+++ public/js/interaction.js
@@ -3,25 +3,10 @@ new Vue({
 
     data: {
         // Model data for the new user form.
-        authencation: {
-            email : '',
-            password : ''
-        },
-
-        newUser: {
-            firstName       : '',
-            lastName        : '',
-            email           : '',
-            password        : '',
-            passwordConfirm : '',
-            address         : '',
-            country         : '',
-            city            : '',
-            postal          : '',
-            phone           : '',
-            mobilePhone     : ''
-        },
-
+        authencation : { email: '', password: '' },
+        newUser      : { firstName: '', lastName: '', email: '', password: '', passwordConfirm : '', address: '', country: '', city: '', postal: '', phone: '', mobilePhone: ''},
+        holiday      : { from: '', type: '', until: '' },
+        config       : { email: '', title: '', date: '', time: '' }
     },
 
     // Compouted error models:
@@ -35,6 +20,24 @@ new Vue({
             return false;
         },
 
+        // Update the basic config.
+        errorsBasicConfig: function() {
+            for (var kay in thit.config) {
+                if ( ! this.config[key]) return true;
+            }
+
+            return false;
+        },
+
+        errorsHolidayRequest: function() {
+            for (var key in this.holiday) {
+                if ( ! this.holiday[key]) return true;
+            }
+
+            return false;
+        },
+
+        // Errors on the login screen.
         errorsAuthencation: function() {
             for (var key in this.authencation) {
                 if ( ! this.authencation[key]) return true;
--- resources/views/holidays/request.blade.php
+++ resources/views/holidays/request.blade.php
@@ -1,63 +1,66 @@
 @extends('header')
 @section('content')
-<div class="page-header">
-<h2>Holiday requests</h2>
-</div>
+    <div class="page-header">
+        <h2>Holiday requests</h2>
+    </div>
 
-<div class="col-md-12">
- <div class='block block-size-medium'>
- <div class='block-content-outer'>
- <div class='block-content-inner'>
-  <div class="container-fluid">
+    <div class="col-md-12">
+        <div class='block block-size-medium'>
+            <div class='block-content-outer'>
+                <div class='block-content-inner'>
+                    <div class="container-fluid">
 
-{!! Form::model(array('url' => "sick_notification/register", 'method' => "POST")) !!}
-{!! csrf_field() !!}
+                        {!! Form::model(array('url' => "sick_notification/register", 'method' => "POST")) !!}
+                        {!! csrf_field() !!}
 
-<div class="form-group">
- <label for="type" class="form-label">{{Lang::get('tasks.schedule_type')}} <span class="text-danger">*</span></label>
- <select id="type" name="type" class="form-control">
-  <option selected=""></option>
-   <option value=""></option>
- </select>
-</div>
+                        <div class="form-group">
+                            <label for="type" class="form-label">
+                                {{ Lang::get('tasks.schedule_type') }}
+                                <span v-if="! holiday.type" class="text-danger">*</span>
+                            </label>
+                            <select v-model="holiday.type" id="type" name="type" class="form-control">
+                                <option selected=""></option>
+                                <option value=""></option>
+                            </select>
+                        </div>
 
-<div class="form-group">
- <label for="start_date" class="form-label">{{Lang::get('tasks.taskStarts')}} <span class="text-danger">*</span></label>
- <input type='text' id="start_date" name="start_date" class="form-control" />
- </div>
+                        <div class="form-group">
+                            <label for="start_date" class="form-label">
+                                {{ Lang::get('tasks.taskStarts') }}
+                                <span v-if="! holiday.from" class="text-danger">*</span>
+                            </label>
+                            <input v-model="holiday.from" type='text' id="start_date" name="start_date" class="form-control"/>
+                        </div>
 
-<div class="form-group">
-  <label for="stop_date" class="form-label">{{Lang::get('tasks.taskStops')}} <span class="text-danger">*</span></label>
-  <input type="text" id="stop_date" name="stop_date" class="form-control">
-</div>
+                        <div class="form-group">
+                            <label for="stop_date" class="form-label">
+                                {{Lang::get('tasks.taskStops')}}
+                                <span v-if="! holiday.until" class="text-danger">*</span>
+                            </label>
+                            <input v-model="holiday.until" type="text" id="stop_date" name="stop_date" class="form-control">
+                        </div>
 
-<div class="form-group">
- <label for="description" class="form-label">{{Lang::get('tasks.description')}}</label>
- <textarea id="description" name="description" rows="10" class="form-control"></textarea>
-</div>
+                        <div class="form-group">
+                            <label for="description" class="form-label">{{Lang::get('tasks.description')}}</label>
+                            <textarea id="description" name="description" rows="10" class="form-control"></textarea>
+                        </div>
 
-<div class="form-group">
- <label for="save" class="form-label">&nbsp;</label>
- <button type="submit" name="save" class="btn btn-primary">Save</button>
- <button type="reset" name="reset" class="btn btn-danger">Cancel</button>
-</div>
- 
- </div>
- </div>
- </div>
- </div>
+                        <div class="form-group">
+                            <label for="save" class="form-label">&nbsp;</label>
+                            <button :disabled="errorsHolidayRequest" type="submit" name="save" class="btn btn-primary">Save</button>
+                            <button type="reset" name="reset" class="btn btn-danger">Cancel</button>
+                        </div>
 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
-        <script type="text/javascript">
-            $(function () {
-
-              $('#start_date').datetimepicker({
-
-              });
-
-              $('#stop_date').datetimepicker({
-
-              });
-            });
-        </script>
- @endsection
+    <script type="text/javascript">
+        $(function () {
+            $('#start_date').datetimepicker({});
+            $('#stop_date').datetimepicker({});
+        });
+    </script>
+@endsection
